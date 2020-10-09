629548c983dd021b0e7121dcfa166986912626b0
Kevin Tou <toukevin@gmail.com>
2020-10-09 16:45:37 -0700

    Added basic fields to form

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
+import { Container } from '@material-ui/core';
 
 // Components
 import Login from './Login';
@@ -7,14 +8,14 @@ import EmailForm from './EmailForm';
 
 function App() {
   return (
-    <>
+    <Container component="main" maxWidth="xs">
       <Switch>
         <Route exact path="/" component={Login} />
         {/* Form should be a protected route! */}
         <Route path="/form" component={EmailForm} />
         <Redirect to="/" />
       </Switch>
-    </>
+    </Container>
   );
 }
 
--- src/components/EmailForm.js
+++ src/components/EmailForm.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  form: {
+    width: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+    marginTop: theme.spacing(3, 0 ,2)
+  }
+}));
 
 const EmailForm = () => {
+  const { form } = useStyles();
+
   return (
-    <form>
-      <p>Some Text</p>
+    <form className={form}>
+      <TextField id="name-input" label="Name" variant="outlined" required />
+      <TextField id="email-input" label="Email" variant="outlined" required />
+      <TextField id="message-input" label="Message" variant="outlined" required />
+      <Button>Submit</Button>
     </form>
   )
 }
